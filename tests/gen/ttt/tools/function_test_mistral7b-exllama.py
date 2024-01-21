d1a7a949aeac2bc2b210dfa40e7b2d3769d28f1b
RoyLai-InfoCorp <roy.lai@infocorp.io>
2024-01-21 17:32:49 +0800

    Tweaked prompt to work for long context

--- tests/gen/ttt/tools/function_test_mistral7b-exllama.py
+++ tests/gen/ttt/tools/function_test_mistral7b-exllama.py
@@ -6,13 +6,13 @@ with open("./tests/gen/ttt/tools/tools.txt","r") as f:
     tools = json.load(f)
 
 # # This call does not involve tools
-# with open("./tests/gen/ttt/tools/short_text.txt","r") as f:
-#         context = f.read()
-# messages = chat_string_to_list(context)
-# print("\nText Prompt")
-# response = g.create(messages=messages,stream=True,max_new_tokens=1000)
-# for chunk in response:
-#     print(chunk.choices[0].delta.content,end="",flush=True)
+with open("./tests/gen/ttt/tools/short_text.txt","r") as f:
+        context = f.read()
+messages = chat_string_to_list(context)
+print("\nText Prompt")
+response = g.create(messages=messages,stream=True,max_new_tokens=1000)
+for chunk in response:
+    print(chunk.choices[0].delta.content,end="",flush=True)
 
 
 #This call should not invoke the tools
@@ -25,30 +25,30 @@ for chunk in response:
     print(chunk.choices[0].delta.content,end="",flush=True)
 
 #This call should invoke the tools
-# with open("./tests/gen/ttt/tools/long_text.txt","r") as f:
-#         context = f.read()
-# messages = chat_string_to_list(context)
-# print("\nTools Prompt with Tools")
-# response = g.create(messages=messages,stream=True,max_new_tokens=1000,tools=tools)
-# for chunk in response:
-#     if chunk.choices[0].delta.content:
-#         print("CONTENT")
-#         print(chunk.choices[0].delta.content)
-#     if chunk.choices[0].delta.tool_calls and chunk.choices[0].delta.tool_calls[0].function.name:
-#         print("NAME")
-#         print(chunk.choices[0].delta.tool_calls[0].function.name)
-#     if chunk.choices[0].delta.tool_calls and chunk.choices[0].delta.tool_calls[0].function.arguments:
-#         print("ARGUMENTS")
-#         print(chunk.choices[0].delta.tool_calls[0].function.arguments)
+with open("./tests/gen/ttt/tools/long_text.txt","r") as f:
+        context = f.read()
+messages = chat_string_to_list(context)
+print("\nTools Prompt with Tools")
+response = g.create(messages=messages,stream=True,max_new_tokens=1000,tools=tools)
+for chunk in response:
+    if chunk.choices[0].delta.content:
+        print("CONTENT")
+        print(chunk.choices[0].delta.content)
+    if chunk.choices[0].delta.tool_calls and chunk.choices[0].delta.tool_calls[0].function.name:
+        print("NAME")
+        print(chunk.choices[0].delta.tool_calls[0].function.name)
+    if chunk.choices[0].delta.tool_calls and chunk.choices[0].delta.tool_calls[0].function.arguments:
+        print("ARGUMENTS")
+        print(chunk.choices[0].delta.tool_calls[0].function.arguments)
 
 
 #This call should invoke the tools even though it contains irrelevant context
-# with open("./tests/gen/ttt/tools/long_irrelevant_text.txt","r") as f:
-#         context = f.read()
-# messages = chat_string_to_list(context)
-# print("\nTools Prompt with Tools and Context")
-# response = g.create(messages=messages,stream=True,max_new_tokens=1000,tools=tools)
-# for chunk in response:
-#     if chunk.choices[0].delta.tool_calls and chunk.choices[0].delta.tool_calls[0].function.arguments:
-#         print("ARGUMENTS")
-#         print(chunk.choices[0].delta.tool_calls[0].function.arguments)
+with open("./tests/gen/ttt/tools/long_irrelevant_text.txt","r") as f:
+        context = f.read()
+messages = chat_string_to_list(context)
+print("\nTools Prompt with Tools and Context")
+response = g.create(messages=messages,stream=True,max_new_tokens=1000,tools=tools)
+for chunk in response:
+    if chunk.choices[0].delta.tool_calls and chunk.choices[0].delta.tool_calls[0].function.arguments:
+        print("ARGUMENTS")
+        print(chunk.choices[0].delta.tool_calls[0].function.arguments)
--- tests/gen/ttt/tools/function_test_mistral7b_128k-exllama.py
+++ tests/gen/ttt/tools/function_test_mistral7b_128k-exllama.py
@@ -6,19 +6,27 @@ with open("./tests/gen/ttt/tools/tools.txt","r") as f:
         tools = json.load(f)
 
 # This call should not invoke the tools
-with open("./tests/gen/ttt/tools/short_text.txt","r") as f:
-        context = f.read()
-messages = chat_string_to_list(context)
-print("\nshort context")
-response = g.create(messages=messages,stream=True,max_new_tokens=1000,tools=tools)
-for chunk in response:
-    print(chunk.choices[0].delta.content,end="",flush=True)
+# with open("./tests/gen/ttt/tools/short_text.txt","r") as f:
+#         context = f.read()
+# messages = chat_string_to_list(context)
+# print("\nshort context")
+# response = g.create(messages=messages,stream=True,max_new_tokens=1000,tools=tools)
+# for chunk in response:
+#     print(chunk.choices[0].delta.content,end="",flush=True)
 
 # This call should invoke the tools
 with open("./tests/gen/ttt/tools/long_text.txt","r") as f:
         context = f.read()
 messages = chat_string_to_list(context)
-print("\nlong context")
+print("\nTools Prompt with Tools")
 response = g.create(messages=messages,stream=True,max_new_tokens=1000,tools=tools)
 for chunk in response:
-    print(chunk.choices[0].delta.content,end="",flush=True)
+    if chunk.choices[0].delta.content:
+        print("CONTENT")
+        print(chunk.choices[0].delta.content)
+    if chunk.choices[0].delta.tool_calls and chunk.choices[0].delta.tool_calls[0].function.name:
+        print("NAME")
+        print(chunk.choices[0].delta.tool_calls[0].function.name)
+    if chunk.choices[0].delta.tool_calls and chunk.choices[0].delta.tool_calls[0].function.arguments:
+        print("ARGUMENTS")
+        print(chunk.choices[0].delta.tool_calls[0].function.arguments)
